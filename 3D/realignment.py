3caea591d6062a2ddc9caf75fa20a08de7012439
Benjamin Roter <benjaminroter2026@u.northwestern.edu>
2025-02-28 22:50:03 +0000

    BR

--- 3D/realignment.py
+++ 3D/realignment.py
@@ -1,4 +1,4 @@
-import numpy as np, h5py, os, skimage, tkinter as tk
+import numpy as np, h5py, os, skimage, tkinter as tk, tomopy as tomo
 
 from skimage import transform as xform
 from scipy import signal as sig
@@ -6,9 +6,9 @@ from h5_util import extract_h5_aggregate_xrf_data, create_aggregate_xrf_h5
 from matplotlib import pyplot as plt
 from tkinter import filedialog
         
-def iter_reproj(ref_element, element_array, xrf_proj_img_array):
+def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array):
     sinogram = np.zeros((xrf_proj_img_array.shape[1], xrf_proj_img_array.shape[2], xrf_proj_img_array.shape[3]))
-    reconstructed_slice_array = []
+    # reconstructed_slice_array = []
 
     ref_element_idx = element_array.index(ref_element)
     
@@ -16,14 +16,8 @@ def iter_reproj(ref_element, element_array, xrf_proj_img_array):
 
     for slice_idx in range(xrf_proj_img_array.shape[2]):
         sinogram[:, slice_idx, :] = reference_projection_imgs[:, slice_idx, :]
-        
-        reconstructed_slice = xform.iradon(sinogram[:, slice_idx, :], filter_name='ramp', circle=True)
 
-        reconstructed_slice_array.append(reconstructed_slice)
-    
-    reconstructed_object = np.stack(reconstructed_slice_array, axis = 1)
-
-    plt.imshow(reconstructed_object[:, 40, :])
+    plt.imshow(sinogram[:, xrf_proj_img_array.shape[2]//2, :])
     plt.show()
 
 
@@ -39,16 +33,15 @@ root = tk.Tk()
     
 root.withdraw()
 
-input_xrf_file_path_array = filedialog.askopenfilenames(parent = root, title = "Select XRF HDF5 files", filetypes = [("HDF5 files", "*.h5")])
-# file_path_xrt = ''
 
-create_aggregate_xrf_h5(input_xrf_file_path_array)
+# elements_xrf, counts_xrf, theta_xrf, dataset_type_xrf = extract_h5_aggregate_xrf_data(file_path_xrf)
 
-file_path_xrf = filedialog.askopenfilenames(parent = root, title = "Select aggregate XRF HDF5 file", filetypes = [("HDF5 files", "*.h5")])
+file_path_xrf = '/home/bwr0835/2_ide_aggregate_xrf.h5'
+# # file_path_xrt = ''
 
 elements_xrf, counts_xrf, theta_xrf, dataset_type_xrf = extract_h5_aggregate_xrf_data(file_path_xrf)
 
-iter_reproj('Fe', elements_xrf, counts_xrf)
+iter_reproj('Fe', elements_xrf, theta_xrf, counts_xrf)
 
 
 
