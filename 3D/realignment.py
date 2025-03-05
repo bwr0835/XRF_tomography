d983e8548659f2c95f4549cc32cba6e300e25fee
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-03-04 20:21:14 -0600

    Update realignment.py

--- 3D/realignment.py
+++ 3D/realignment.py
@@ -60,10 +60,11 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                 print(recon.shape)
             
                 for slice_idx in range(n_slices):
+                    print('Slice ' + str(slice_idx + 1) + '/' + str(n_slices))
                     proj_slice = recon[element_idx, slice_idx, :, :]
                     proj_imgs_from_3d_recon[element_idx, :, slice_idx, :] = np.rot90(skimage.transform.radon(proj_slice, theta = theta_array), k = 1)
 
-        mse = skimage.metrics.mean_squared_error(proj_imgs_from_3d_recon[ref_element], reference_projection_imgs)/(n_theta*n_columns) # MSE (for convergence)
+        mse = skimage.metrics.mean_squared_error(proj_imgs_from_3d_recon[ref_element_idx], reference_projection_imgs)/(n_theta*n_columns) # MSE (for convergence)
 
         print(mse)
 
