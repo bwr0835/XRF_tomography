0fc6432ec2b0794d46f7c1398a8c582ffa4ba11f
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-03-18 14:27:58 -0500

    Update realignment.py

--- 3D/realignment.py
+++ 3D/realignment.py
@@ -11,6 +11,128 @@ plt.rcParams['text.usetex'] = True
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['text.latex.preamble'] = r'\usepackage{times}'
 
+def edge_gauss_filter(image, sigma, alpha, nx, ny):
+    import numpy as np
+
+    n_rolloff = int(0.5 + alpha*sigma)
+    
+    if n_rolloff > 2:
+        exp_arg =  np.arange(n_rolloff)/float(sigma)
+
+        rolloff = 1 - np.exp(-0.5*exp_arg**2)
+    
+    edge_total = 0
+        
+    # Bottom edge
+ 
+    y1 = ny - sigma
+    y2 = ny
+
+    edge_total = edge_total + np.sum(image[y1:y2, 0:nx])
+
+    # Top edge
+        
+    y3 = 0
+    y4 = sigma
+        
+        
+    edge_total = edge_total + np.sum(image[y3:y4, 0:nx])
+
+    # Left edge
+
+    x1 = 0
+    x2 = sigma
+
+    edge_total = edge_total + np.sum(image[y4:y1, x1:x2])
+
+    # Right edge
+
+    x3 = nx - sigma
+    x4 = nx
+
+    edge_total = edge_total + np.sum(image[y4:y1, x3:x4])
+
+    n_pixels = 2*sigma*(nx + ny - 2*sigma) # Total number of edge pixels for this "vignetting"
+                                           # sigma*nx + sigma*nx + [(ny - sigma) - sigma]*sigma + (ny - sigma) - sigma]*sigma
+    
+    dc_value = edge_total/n_pixels # Average of edge_total over total number of edge pixels
+
+    image = np.copy(image) - dc_value
+    
+    # Top edge
+
+    xstart = 0
+    xstop = nx - 1
+    idy = 0
+
+    for i_rolloff in range(n_rolloff):
+        image[idy, xstart:(xstop+1)] = image[idy, xstart:(xstop+1)]*rolloff[idy]
+        
+        if xstart < (nx/2 - 1):
+            xstart += 1
+        
+        if xstop > (nx/2):
+            xstop -= 1
+        
+        if idy < (ny - 1):
+            idy += 1
+
+    # Bottom edge
+    
+    xstart = 0
+    xstop = nx - 1
+    idy = ny - 1
+
+    for i_rolloff in range(n_rolloff):
+        image[idy, xstart:(xstop+1)] = image[idy, xstart:(xstop+1)]*rolloff[ny - 1 - idy]
+
+        if xstart < (nx/2 - 1):
+            xstart += 1
+        
+        if xstop > nx/2:
+            xstop -= 1
+        
+        if idy > 0:
+            idy -= 1
+
+    # Left edge
+
+    ystart = 1
+    ystop = ny - 2
+    idx = 0
+
+    for i_rolloff in range(n_rolloff):
+        image[ystart:(ystop+1), idx] = image[ystart:(ystop+1), idx]*rolloff[idx]
+
+        if ystart < (ny/2 - 1):
+            ystart += 1
+       
+        if ystop > (ny/2):
+            ystop -= 1
+        
+        if idx < (nx - 1):
+            idx += 1
+
+    # Right edge
+
+    ystart = 1
+    ystop = ny - 2
+    idx = nx - 1
+
+    for i_rolloff in range(n_rolloff):
+        image[ystart:(ystop+1), idx] = image[ystart:(ystop+1), idx]*rolloff[nx - 1 - idx]
+
+        if ystart < (ny/2 - 1):
+            ystart += 1
+        
+        if ystop > (ny/2):
+            ystop -= 1
+        
+        if idx > 0:
+            idx -= 1
+    
+    return (image + dc_value)
+
 def ramp_filter(sinogram):
     n_theta, n_rows, n_columns = sinogram.shape
     
@@ -61,7 +183,7 @@ def cross_correlate(recon_proj, orig_proj):
     return y_shift, x_shift, cross_corr
 
 def phase_correlate(recon_proj, orig_proj, upsample_factor):
-    shift, error, diffphase = reg.phase_cross_correlation(reference_image = recon_proj, moving_image = orig_proj, upsample_factor = upsample_factor)
+    shift, _, _ = reg.phase_cross_correlation(reference_image = recon_proj, moving_image = orig_proj, upsample_factor = upsample_factor)
 
     y_shift, x_shift = shift[0], shift[1]
 
@@ -160,7 +282,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
     ref_element_idx = element_array.index(ref_element)
     reference_projection_imgs = xrf_proj_img_array[ref_element_idx] # These are effectively sinograms for the element of interest (highest contrast -> for realignment purposes)
                                                                     # (n_theta, n_slices -> n_rows, n_columns)
-    sr_trans = sr(sr.TRANSLATION)
+    # sr_trans = sr(sr.TRANSLATION)
 
     center_of_rotation = tomo.find_center(reference_projection_imgs, theta_array*np.pi/180)
 
@@ -219,7 +341,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
 
         for theta_idx in range(n_theta):
             y_shift_cc, x_shift_cc, corr_mat_cc = cross_correlate(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx]) # Cross-correlation
-            y_shift_pc, x_shift_pc = phase_correlate(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx], upsample_factor = 25)
+            y_shift_pc, x_shift_pc = phase_correlate(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx], upsample_factor = 1)
             # y_shift_pc, x_shift_pc = subpixel_cross_correlation(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx])
             
             # save_proj_img_npy(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], iteration_idx, theta_array[theta_idx], 'synthesized', 'gridrec', output_dir_path)
@@ -243,7 +365,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                 x_shifts_pc[iteration_idx, theta_idx] = x_shifts_pc[iteration_idx - 1, theta_idx] + x_shift_pc
                 y_shifts_pc[iteration_idx, theta_idx] = y_shifts_pc[iteration_idx - 1, theta_idx] + y_shift_pc
 
-            if (theta_idx % 7 == 0) or (theta_array[theta_idx] == -22):
+            if theta_idx % 7 == 0:
                 print('x-shift: ' + str(x_shift_pc) + ' (Theta = ' + str(theta_array[theta_idx]) + ' degrees')
                 print('y-shift: ' + str(y_shift_pc))
 
@@ -297,9 +419,9 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                     x_shift = x_shifts_pc[iteration_idx, theta_idx]
                     y_shift = y_shifts_pc[iteration_idx, theta_idx]
                     
-                    aligned_proj[element_idx, theta_idx, :, :] = sr_trans.register_transform(ref = proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], mov = reference_projection_imgs[theta_idx])
+                    # aligned_proj[element_idx, theta_idx, :, :] = sr_trans.register_transform(ref = proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], mov = reference_projection_imgs[theta_idx])
 
-                    # aligned_proj[element_idx, theta_idx, :, :] = spndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], shift = (y_shift, x_shift), cval = 0) # Undo the translational shifts by the cross-correlation peak
+                    aligned_proj[element_idx, theta_idx, :, :] = spndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], shift = (y_shift, x_shift), cval = 0) # Undo the translational shifts by the cross-correlation peak
 
         current_xrf_proj_img_array = aligned_proj.copy()
 
