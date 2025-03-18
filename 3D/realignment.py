78f0b4987297ab9951f28630a725c797406b033c
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-03-18 13:15:02 -0500

    Update realignment.py

--- 3D/realignment.py
+++ 3D/realignment.py
@@ -297,7 +297,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                     x_shift = x_shifts_pc[iteration_idx, theta_idx]
                     y_shift = y_shifts_pc[iteration_idx, theta_idx]
                     
-                    aligned_proj[element_idx, theta_idx, :, :] = sr.register_transform(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx])
+                    aligned_proj[element_idx, theta_idx, :, :] = sr.register_transform(ref = proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], mov = reference_projection_imgs[theta_idx])
 
                     # aligned_proj[element_idx, theta_idx, :, :] = spndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], shift = (y_shift, x_shift), cval = 0) # Undo the translational shifts by the cross-correlation peak
 
