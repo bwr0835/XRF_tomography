962cee29947000cf6d3866ef731df0e5ecd80e4d
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-03-19 11:12:31 -0500

    Update realignment_test.py

--- 3D/realignment_test.py
+++ 3D/realignment_test.py
@@ -195,8 +195,8 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                 # aligned_proj[element_idx, theta_idx, :, :] = xform.warp(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], tform, preserve_range = True) # Undo the translational shifts by the cross-correlation peak
                 aligned_proj[ref_element_idx, theta_idx, :, :] = spndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], shift = (y_shift, x_shift)) # Undo the translational shifts by the cross-correlation peak
 
-        else:  
-            aligned_proj = xrf_proj_img_array
+        else:
+            aligned_proj = xrf_proj_img_array.copy()
         
         algorithm = 'gridrec'
 
@@ -224,7 +224,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
     
         for theta_idx in range(n_theta):
             y_shift_cc, x_shift_cc, corr_mat_cc = cross_correlate(proj_imgs_from_3d_recon[theta_idx, :, :], aligned_proj[ref_element_idx, theta_idx, :, :]) # Cross-correlation
-            y_shift_pc, x_shift_pc = phase_correlate(proj_imgs_from_3d_recon[theta_idx, :, :], aligned_proj[ref_element_idx, theta_idx, :, :], upsample_factor = 25)
+            y_shift_pc, x_shift_pc = phase_correlate(proj_imgs_from_3d_recon[theta_idx, :, :], aligned_proj[ref_element_idx, theta_idx, :, :], upsample_factor = 50)
             # y_shift_pc, x_shift_pc = subpixel_cross_correlation(proj_imgs_from_3d_recon[ref_element_idx, theta_idx, :, :], reference_projection_imgs[theta_idx])
             
             x_shift_pc_array[theta_idx] = x_shift_pc
@@ -271,8 +271,8 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
                 y_shifts_pc[iteration_idx, theta_idx] = y_shift_pc_array[theta_idx]
             
             else:
-                x_shifts_pc[iteration_idx, theta_idx] = x_shift_pc[iteration_idx - 1, theta_idx] + x_shift_pc_array[theta_idx]
-                y_shifts_pc[iteration_idx, theta_idx] = y_shift_pc[iteration_idx - 1, theta_idx] + y_shift_pc_array[theta_idx]
+                x_shifts_pc[iteration_idx, theta_idx] = x_shifts_pc[iteration_idx - 1, theta_idx] + x_shift_pc_array[theta_idx]
+                y_shifts_pc[iteration_idx, theta_idx] = y_shifts_pc[iteration_idx - 1, theta_idx] + y_shift_pc_array[theta_idx]
 
 
         
