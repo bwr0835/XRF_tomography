36868bdb2c9bf8e1c804fa5e0bbe1b53b886c63b
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-03-19 15:28:42 -0500

    Update tomo_movie.py

--- 3D/tomo_movie.py
+++ 3D/tomo_movie.py
@@ -163,8 +163,8 @@ else:
 
     iter_array = 1 + np.arange(n_iterations)
 
-    curve1 = axs3.plot(iter_array, np.zeros_like(iter_array), 'k-o', markersize = 3, label = r'$\Delta x$')[0]
-    curve2 = axs3.plot(iter_array, np.zeros_like(iter_array), 'r-o', markersize = 3, label = r'$\Delta y$')[0]
+    curve1, = axs3.plot(iter_array, np.zeros_like(iter_array), 'k-o', markersize = 3, label = r'$\Delta x$')
+    curve2, = axs3.plot(iter_array, np.zeros_like(iter_array), 'r-o', markersize = 3, label = r'$\Delta y$')
 
     axs1[0].set_title(r'Recon. Slice (It. 1)')
     axs1[1].set_title(r'Recon. Slice (It. {0})'.format(n_iterations))
@@ -259,8 +259,6 @@ else:
     
     def animate_shifts(frame):
         artists = []
-
-        axs3.cla()
         
         net_shift_x = x_shifts_data[:, frame]
         net_shift_y = y_shifts_data[:, frame]
