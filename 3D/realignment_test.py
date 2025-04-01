614e56c704b5b66200b8bcf22038187af18e2196
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-04-01 14:38:01 -0500

    Update realignment_test.py

--- 3D/realignment_test.py
+++ 3D/realignment_test.py
@@ -427,7 +427,7 @@ def iter_reproj(ref_element, element_array, theta_array, xrf_proj_img_array, n_i
             # save_recon_slice_npy(proj_slice, iteration_idx, slice_idx, 'gridrec', output_dir_path)
 
         for theta_idx in range(n_theta):
-            if iteration_idx == 0:
+            if iteration_idx > 0:
             # y_shift_pc, x_shift_cc, corr_mat_cc = cross_correlate(proj_imgs_from_3d_recon[theta_idx, :, :], aligned_proj[ref_element_idx, theta_idx, :, :]) # Cross-correlation
                 y_shift_pc, x_shift_pc = phase_correlate(proj_imgs_from_3d_recon[theta_idx, :, :], ndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx, :, :], shift = (y_shift, x_shift)), upsample_factor = 100)
 
