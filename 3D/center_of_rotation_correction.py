d768b72b2286758516a958882589dee5e12d0ee8
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-04-15 16:08:22 -0500

    Update center_of_rotation_correction.py

--- 3D/center_of_rotation_correction.py
+++ 3D/center_of_rotation_correction.py
@@ -175,6 +175,7 @@ def create_save_proj_shifts(elements_xrf, counts_xrf, theta_xrf, ref_element, co
             counts_new[theta_idx] = ndi.shift(counts[theta_idx], shift = (0, cor_x_shift[x_shift]))
         
         if x_shift % 10 == 0:
+            plt.clf()
             plt.imshow(counts_new[theta_idx])
             plt.show()
 
