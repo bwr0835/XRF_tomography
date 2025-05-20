303e79d8a8828863226a4a643017fca9e7396c59
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-05-20 09:55:48 -0500

    Create recon_compare.py

---
+++ 3D/recon_compare.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+file_1 = '/Users/bwr0835/iter_reproj/mlem_1_iter_manual_shift_20_tomopy_default_cor_w_padding_05_06_2025/aligned_proj_array_iter_Fe.npy'
+file_2 = '/Users/bwr0835/iter_reproj/gridrec_1_iter_manual_shift_20_tomopy_default_cor_w_padding_05_06_2025/aligned_proj_array_iter_Fe.npy'
+file_3 = '/Users/bwr0835/iter_reproj/gridrec_1_iter_no_cor_shift_tomopy_default_cor_w_padding_05_06_2025/aligned_proj_array_iter_Fe.npy'
+file_4 = ''
