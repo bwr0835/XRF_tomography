8be3ba7a71956ad300aeeb6a9d673718c420207a
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-05-22 14:30:28 -0500

    Update h5_util.py

--- 3D/h5_util.py
+++ 3D/h5_util.py
@@ -1,4 +1,4 @@
-import numpy as np, h5py
+import numpy as np, h5py, os
 
 def extract_h5_xrf_data(file_path, synchrotron):
     h5 = h5py.File(file_path, 'r')
@@ -14,7 +14,6 @@ def extract_h5_xrf_data(file_path, synchrotron):
                 
             counts = counts_h5[()]
             elements = elements_h5[()]
-                
             extra_pvs_names = extra_pvs_h5['Names'][()]
             extra_pvs_values = extra_pvs_h5['Values'][()]
 
@@ -106,34 +105,127 @@ def extract_h5_xrf_data(file_path, synchrotron):
 
         return elements_string, counts, theta, nx, ny, dx_cm, dy_cm
 
+def extract_h5_xrt_data(file_path, synchrotron):
+    h5 = h5py.File(file_path, 'r')
+
+    if synchrotron == "Advanced Photon Source (APS)" or synchrotron == "APS" or synchrotron == "aps" or synchrotron == "Advanced Photon Source" or synchrotron == "advanced photon source" or synchrotron == "advanced photon source (aps)":
+        scalers_h5 = h5['MAPS/Scalers']
+        extra_pvs_h5 = h5['MAPS/Scan/Extra_PVs']
+        nx_h5 = h5['MAPS/Scan/x_axis']
+        ny_h5 = h5['MAPS/Scan/y_axis']
+        
+        scaler_names = scalers_h5['Names'][()]
+        scaler_values = scalers_h5['Values'][()]
+        extra_pvs_names = extra_pvs_h5['Names'][()]
+        extra_pvs_values = extra_pvs_h5['Values'][()]
+        nx_conv = ny_h5[()] # Width and height are reversed in the actual HDF5 data structure
+        ny_conv = nx_h5[()] # Width and height are reversed in the actual HDF5 data structure
+        
+        elements = ['empty', 'us_ic', 'ds_ic', 'abs_ic']
+        n_elements = len(elements)
+
+        nx = len(nx_conv)
+        ny = len(ny_conv) - 2 # MAPS tacks on two extra values for whatever reason
+        
+        us_ic_idx = np.where(scaler_names == b'US_IC')[0]
+        ds_ic_idx = np.where(scaler_names == b'DS_IC')[0]
+        abs_ic_idx = np.where(scaler_names == b'abs_ic')[0]
+        theta_idx = np.where(extra_pvs_names == b'2xfm:m58.VAL')[0]
+
+        nx, ny = ny, nx
+        
+        cts_combined = np.zeros((n_elements, ny, nx))
+        
+        cts_us_ic = scaler_values[us_ic_idx][:, :-2] # Remove last two columns since they are added after row is finished scanning
+        cts_ds_ic = scaler_values[ds_ic_idx][:, :-2]
+        cts_abs_ic = scaler_values[abs_ic_idx][:, :-2]
+
+        cts_combined[1] = cts_us_ic
+        cts_combined[2] = cts_ds_ic
+        cts_combined[3] = cts_abs_ic
+
+        dx_cm = 1e-1*np.abs([-1] - nx_conv[0])/(nx - 1)
+        dy_cm = 1e-1*np.abs(ny_conv[-3] - ny_conv[0])/(ny - 1)
+
+        theta = float(extra_pvs_values[theta_idx][0].decode())
+
+        return elements, cts_combined, theta, nx, ny, dx_cm, dy_cm
+
 def create_aggregate_xrf_h5(file_path_array, output_h5_file, synchrotron):
-    
     n_theta = len(file_path_array)
 
     theta_array = np.zeros(n_theta) 
 
-    elements, counts, theta, nx, ny, dx_cm, dy_cm = extract_h5_xrf_data(file_path_array[0], synchrotron) # Invoke the first time for getting the number of elements and the number of pixels
+    elements, counts, theta, nx, ny, _, _ = extract_h5_xrf_data(file_path_array[0], synchrotron) # Invoke the first time for getting the number of elements and the number of pixels
     
     n_elements = len(elements)
     
     counts_array = np.zeros((n_elements, n_theta, ny, nx))
 
     for theta_idx, file_path in enumerate(file_path_array):
-        elements_new, counts, theta, nx_new, ny_new, dx_cm, dy_cm = extract_h5_xrf_data(file_path, synchrotron)
+        elements_new, counts, theta, nx_new, ny_new, _, _ = extract_h5_xrf_data(file_path, synchrotron)
         
         assert nx == nx_new and ny == ny_new, f"Dimension mismatch in {file_path}." # Check that the dimensions of the new data match the dimensions of the first data set
         assert np.array_equal(elements, elements_new), f"Element mismatch in {file_path}." # Check that the elements are the same
         
         counts_array[:, theta_idx, :, :] = counts
         theta_array[theta_idx] = theta
+        file_path_array[theta_idx] = os.path.basename(file_path)
     
+    theta_idx_sorted = np.argsort(theta) # Get indices for angles for sorting them in ascending order
+    
+    theta_array_sorted = theta_array[theta_idx_sorted]
+    counts_array_sorted = counts_array[:, theta_idx_sorted, :, :]
+    file_path_array_sorted = file_path_array[theta_idx_sorted]
+
     with h5py.File(output_h5_file, 'w') as f:
         exchange = f.create_group('exchange')
+        file_info = f.creategroup('corresponding_file_info')
 
-        exchange.create_dataset('data', data = counts_array, compression = 'gzip', compression_opts = 6)
+        exchange.create_dataset('data', data = counts_array_sorted, compression = 'gzip', compression_opts = 6)
         exchange.create_dataset('elements', data = elements_new)
-        exchange.create_dataset('theta', data = theta_array)
-        exchange.create_dataset('dataset_type', data = 'xrf')
+        exchange.create_dataset('theta', data = theta_array_sorted)
+        
+        file_info.create_dataset('filename', data = file_path_array_sorted)
+        file_info.create_dataset('dataset_type', data = 'xrf')
+
+def create_aggregate_xrt_h5(file_path_array, output_h5_file, synchrotron):
+    n_theta = len(file_path_array)
+
+    theta_array = np.zeros(n_theta) 
+
+    elements, counts, theta, nx, ny, _, _ = extract_h5_xrt_data(file_path_array[0], synchrotron) # Invoke the first time for getting the number of elements and the number of pixels
+    
+    n_elements = len(elements)
+    
+    counts_array = np.zeros((n_elements, n_theta, ny, nx))
+
+    for theta_idx, file_path in enumerate(file_path_array):
+        elements_new, counts, theta, nx_new, ny_new, _, _ = extract_h5_xrt_data(file_path, synchrotron)
+        
+        assert nx == nx_new and ny == ny_new, f"Dimension mismatch in {file_path}." # Check that the dimensions of the new data match the dimensions of the first data set
+        assert np.array_equal(elements, elements_new), f"Element mismatch in {file_path}." # Check that the elements are the same
+        
+        counts_array[:, theta_idx, :, :] = counts
+        theta_array[theta_idx] = theta
+        file_path_array[theta_idx] = os.path.basename(file_path)
+    
+    theta_idx_sorted = np.argsort(theta) # Get indices for angles for sorting them in ascending order
+    
+    theta_array_sorted = theta_array[theta_idx_sorted]
+    counts_array_sorted = counts_array[:, theta_idx_sorted, :, :]
+    file_path_array_sorted = file_path_array[theta_idx_sorted]
+
+    with h5py.File(output_h5_file, 'w') as f:
+        exchange = f.create_group('exchange')
+        file_info = f.creategroup('corresponding_file_info')
+
+        exchange.create_dataset('data', data = counts_array_sorted, compression = 'gzip', compression_opts = 6)
+        exchange.create_dataset('elements', data = elements_new)
+        exchange.create_dataset('theta', data = theta_array_sorted)
+        
+        file_info.create_dataset('filenames', data = file_path_array_sorted)
+        file_info.create_dataset('dataset_type', data = 'xrt')
 
 def extract_h5_aggregate_xrf_data(file_path):
     h5 = h5py.File(file_path, 'r')
@@ -148,63 +240,25 @@ def extract_h5_aggregate_xrf_data(file_path):
     elements = elements_h5[()]
     dataset_type = dataset_type_h5[()]
 
-    theta_idx_sorted = np.argsort(theta) # Get indices for angles for sorting them in ascending order
+    elements_string = [element.decode() for element in elements]
 
-    theta_sorted = theta[theta_idx_sorted]
-    counts_sorted = counts[:, theta_idx_sorted, :, :]
+    return elements_string, counts, theta, dataset_type.decode()
+
+def extract_h5_aggregate_xrt_data(file_path):
+    h5 = h5py.File(file_path, 'r')
+    
+    counts_h5 = h5['exchange/data']
+    theta_h5 = h5['exchange/theta']
+    elements_h5 = h5['exchange/elements']
+    dataset_type_h5 = h5['corresponding_file_info']
+
+    counts = counts_h5[()]
+    theta = theta_h5[()]
+    elements = elements_h5[()]
+    dataset_type = dataset_type_h5[()]
 
     elements_string = [element.decode() for element in elements]
 
-    return elements_string, counts_sorted, theta_sorted, dataset_type.decode()
-
-# def extract_h5_aggregate_xrt_data(file_path):
-#     h5 = h5py.File(file_path, 'r')
-    
-#     counts_h5 = h5['exchange/data']
-#     theta_h5 = h5['exchange/theta']
-#     elements_h5 = h5['exchange/elements']
-
-#     counts = counts_h5[()]
-#     theta = theta_h5[()]
-#     elements = elements_h5[()]
-
-#     return elements, counts, theta
-    
-    
-
-# TODO def extract_h5_xrt_data(file_path, synchrotron):
-#     h5 = h5py.File(file_path, 'r')
-
-#     if synchrotron == "Advanced Photon Source (APS)" or synchrotron == "APS" or synchrotron == "aps" or synchrotron == "Advanced Photon Source" or synchrotron == "advanced photon source" or synchrotron == "advanced photon source (aps)":
-#         if "MAPS/XRF_Analyzed/NNLS" in h5.keys():
-
-#     return counts, theta, nx, ny, dx_cm, dy_cm   
-
-# def create_single_xrt_h5(file_path_array, output_h5_file, synchrotron):
-#     n_theta = len(file_path_array) # Each file has a different projection angle theta
-
-#     theta_array = np.zeros(n_theta) 
-
-#     elements, counts, theta, nx, ny, dx_cm, dy_cm = extract_h5_xrt_data(file_path_array[0], synchrotron) # Invoke the first time for getting the number of elements and the number of pixels
-    
-#     n_elements = len(elements)
-
-#     counts_array = np.zeros((n_elements, n_theta, ny, nx))
-
-#     for theta_idx, file_path in enumerate(file_path_array):
-#         elements_new, counts, theta, nx_new, ny_new, dx_cm_new, dy_cm_new = extract_h5_xrt_data(file_path, synchrotron)
-        
-#         assert nx == nx_new and ny == ny_new, f"Dimension mismatch in {file_path}." # Check that the dimensions of the new data match the dimensions of the first data set
-#         assert np.array_equal(elements, elements_new), f"Element mismatch in {file_path}." # Check that the elements are the same
-        
-#         counts_array[:, theta_idx, :, :] = counts
-#         theta_array[theta_idx] = theta
-    
-#     with h5py.File(output_h5_file, 'w') as f:
-#         exchange = f.create_group('exchange')
-
-#         exchange.create_dataset('data', data = counts_array, compression = 'gzip', compression_opts = 6)
-#         exchange.create_dataset('elements', data = elements_new)
-#         exchange.create_dataset('theta', data = theta_array)
+    return elements_string, counts, theta, dataset_type.decode()
 
     
