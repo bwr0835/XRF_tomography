ca0dc56ea2c0efb001dc34b85dd20b4b246ab3aa
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-07-10 15:15:32 -0400

    Update tomo_movie_updated.py

--- 3D/tomo_movie_updated.py
+++ 3D/tomo_movie_updated.py
@@ -368,12 +368,19 @@ synth_slice_proj_intensity_theta_iter_1 = synth_proj_theta_array_aux[0][slice_id
 min_intensity_theta_iter_1 = np.min([np.min(exp_slice_proj_intensity_theta_iter_1), np.min(synth_slice_proj_intensity_theta_iter_1)])
 max_intensity_theta_iter_1 = np.max([np.max(exp_slice_proj_intensity_theta_iter_1), np.max(synth_slice_proj_intensity_theta_iter_1)])
 
-axs9[0].set_xlim(0, n_columns - 1)
-axs9[0].set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
-axs9[0].set_title(r'Iteration index 0')
-axs9[0].set_xlabel(r'Scan position index')
-axs9[0].set_ylabel(r'Intensity (a.u.)')
-axs9[0].legend(frameon = False)
+# axs9[0].set_xlim(0, n_columns - 1)
+# axs9[0].set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
+# axs9[0].set_title(r'Iteration index 0')
+# axs9[0].set_xlabel(r'Scan position index')
+# axs9[0].set_ylabel(r'Intensity (a.u.)')
+# axs9[0].legend(frameon = False)
+
+axs9.set_xlim(0, n_columns - 1)
+axs9.set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
+axs9.set_title(r'Iteration index 0')
+axs9.set_xlabel(r'Scan position index')
+axs9.set_ylabel(r'Intensity (a.u.)')
+axs9.legend(frameon = False)
 
 # axs9[1].set_xlim(0, n_columns - 1)
 # axs9[1].set_title(r'Iteration index {0}'.format(n_iter - 1))
@@ -432,9 +439,9 @@ for theta_idx in range(n_theta):
     # axs5.set_ylim(min_shift, max_shift + 0.1)
     # axs5.set_title(r'$\theta = {0}$\textdegree'.format(theta_array[theta_idx]))
 
-    axs9[0].set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
+    # axs9[0].set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
+    axs9.set_ylim(min_intensity_theta_iter_1, max_intensity_theta_iter_1)
     # axs9[1].set_ylim(min_intensity_theta_iter_final, max_intensity_theta_iter_final)
-    # axs9[1].set_ylim(min_intensity_theta_iter_final, max_intensity_theta_iter_1)
 
     fig9.suptitle(r'$\theta = {0}$\textdegree; Slice index {1}'.format(theta_array[theta_idx], slice_idx_desired))
 
