3b3bff1cdb61800e98301812daf470cc2d56eda0
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-07-29 22:29:19 -0400

    Update realignment_test_2.py

--- 3D/realignment_test_2.py
+++ 3D/realignment_test_2.py
@@ -430,22 +430,22 @@ def iter_reproj(ref_element,
     #     for theta_idx in range(n_theta):
     #         xrf_proj_img_array[element_idx, theta_idx] = ndi.shift(xrf_proj_img_array[element_idx, theta_idx], shift = (0, -offset))
     
-    offset_sum = 0
+    offset_sum = offset.copy()
 
     for i in range(2):
         net_x_shifts_pc[0] -= offset
-        
-        offset_sum += offset
         
         for theta_idx in range(n_theta):
             aligned_proj[theta_idx] = ndi.shift(xrf_proj_img_array[ref_element_idx, theta_idx], shift = (0, -offset))
 
     # center_of_rotation_avg, center_geom, offset = rot_center_avg(xrf_proj_img_array[ref_element_idx], theta_idx_pairs, theta_array)
         center_of_rotation_avg, center_geom, offset = rot_center_avg(aligned_proj, theta_idx_pairs, theta_array)
-    
+
         print(f'New average center of rotation: {round_correct(center_of_rotation_avg, ndec = 3)}')
         print(f'Geometric center: {center_geom}')
         print(f'Center of rotation error: {round_correct(offset, ndec = 3)}')
+
+        offset_sum -= offset
 
     for i in range(n_iterations):
         iterations.append(i)
