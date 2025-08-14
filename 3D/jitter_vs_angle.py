c4824244133f4d6026d4829799d74adfcf49ea85
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-08-14 13:18:26 -0500

    BR

---
+++ 3D/jitter_vs_angle.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+filename = '/Users/bwr0835/Documents/xrt_gridrec_6_iter_dynamic_ps_cor_correction_log_w_padding_gridrec_cor_idx_300_skimage_radon_aug_14_2025/net_x_shifts_ds_ic.npy'
+
+data = np.load(filename)
+
