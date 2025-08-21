83c2f5ead1826e7d88898a029e7d769dc30e0124
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-08-21 12:43:02 -0500

    Update realignment_test_2.py

--- 3D/realignment_test_2.py
+++ 3D/realignment_test_2.py
@@ -698,7 +698,7 @@ def iter_reproj(ref_element,
         print(f'Geometric center: {center_geom}')
         print(f'Center of rotation error: {round_correct(offset_synth, ndec = 3)}')
         
-        if i == 0:
+        if i == 1:
             sys.exit()
         
         if np.max(np.abs(dx_array_pc[i])) < eps and np.max(np.abs(dy_array_pc[i])) < eps:
