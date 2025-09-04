150a9423a5490d6ba65dcda5274ad4ded9bcda06
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-03 20:26:49 -0500

    Create test.py

---
+++ 3D/test.py
@@ -0,0 +1,26 @@
+import numpy as np, h5py
+
+from matplotlib import pyplot as plt
+
+mda_array = ['0117', '0118', '0119', '0120', '0121', '0122', '0123', '0124', '0125', '0126']
+
+for mda, MDA in enumerate(mda_array):
+    try:
+        h5 = h5py.File(f'/raid/users/roter/Jacobsen/img.dat/2xfm_{MDA}.mda.h5')
+
+        extra_pv_names = h5['MAPS/Scan/Extra_PVs/Names'][()]
+        extra_pv_values = h5['MAPS/Scan/Extra_PVs/Values'][()]
+
+        z_idx = np.ndarray.item(np.where(extra_pv_names == b'2xfm:m25.VAL')[0])
+
+        z = extra_pv_values[z_idx]
+
+        print(MDA)
+
+    except:
+        print('None')
+
+ 
+
+
+
