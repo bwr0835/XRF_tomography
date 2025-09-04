e16b66989d3cb78b8bde9809a1664f0eb0256d0a
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-03 20:38:56 -0500

    Update test.py

--- 3D/test.py
+++ 3D/test.py
@@ -11,7 +11,7 @@ for mda, MDA in enumerate(mda_array):
         extra_pv_names = h5['MAPS/Scan/Extra_PVs/Names'][()]
         extra_pv_values = h5['MAPS/Scan/Extra_PVs/Values'][()]
 
-        z_idx = np.ndarray.item(np.where(extra_pv_names == b'2xfm:m26.VAL')[0])
+        z_idx = np.ndarray.item(np.where(extra_pv_names == b'2xfm:m11.VAL')[0])
 
         z = extra_pv_values[z_idx]
 
