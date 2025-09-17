a3044e06e84ae8c5f5c81e24d02a3fe3295a80dc
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-16 23:04:55 -0500

    Update 2_ide_solid_angle.py

--- 3D/2_ide_solid_angle.py
+++ 3D/2_ide_solid_angle.py
@@ -64,19 +64,14 @@ i_tot_us_ic_0126 = us_ic_0126.sum()
 
 dz = [0, 5, 8]
 
-i_tot_xrf = [i_tot_xrf_0124, i_tot_xrf_0125, i_tot_xrf_0126]
-i_tot_us_ic = [i_tot_us_ic_0124, i_tot_us_ic_0125, i_tot_us_ic_0126]
+# i_tot_xrf = [i_tot_xrf_0124, i_tot_xrf_0125, i_tot_xrf_0126]
+# i_tot_us_ic = [i_tot_us_ic_0124, i_tot_us_ic_0125, i_tot_us_ic_0126]
+
+i_tot_xrf = [i_tot_xrf_0126, i_tot_xrf_0125, i_tot_xrf_0124]
+i_tot_us_ic = [i_tot_us_ic_0126, i_tot_us_ic_0125, i_tot_us_ic_0124]
 
 print(i_tot_us_ic)
 print(i_tot_xrf)
-
-sys.exit()
-
-# x_kb, y_kb = dz_kb, i_tot_kb
-# x_cap, y_cap = dz_cap, i_tot_cap
-
-# fit_kb = cf(i_obj_kb, dz_kb, i_tot_kb)
-# mod = Model(i_obj_kb)
 
 mod = Model(i_obj)
 
@@ -84,36 +79,19 @@ params = mod.make_params(A = 1, B = 1)
 params['A'].min = 0
 params['B'].min = 0
 
-# result = mod.fit(i_tot_kb, params, x=dz_kb)
-
-# a_kb = result.params['A'].value
-# a_kb = f"{a_kb:.2e}"
-# a_kb_coeff, a_kb_exponent = a_kb.split("e+0")
-
-# z_det_nom_kb = result.params['B'].value
-# print(z_det_nom_kb)
-
-result = mod.fit(i_tot, params, x = dz)
+result = mod.fit(i_tot_xrf, params, x = dz)
 
 a = result.params['A'].value
 a = f"{a:.2e}"
-a_coeff, a_cap_exponent = a.split("e+0")
+a_coeff, a_exponent = a.split("e+0")
 
 z_det_nom = result.params['B'].value
-
-# print(z_det_nom_cap)
-
-# x1 = np.linspace(np.min(dz_cap), np.max(dz_cap), 1000)
-# yfit = result.eval(x = x1)
 
 x1 = np.linspace(np.min(dz), np.max(dz), 1000)
 yfit = result.eval(x = x1)
 
-plt.scatter(dz, i_tot, color = 'black')
-# plt.scatter(dz_kb, i_tot_kb, color = 'black')
-# plt.plot(x1, yfit, 'r--', label = r'$I = {0}/\left({1} + \Delta d\right)^{{2}}$'.format(rc(a_kb, ndec = 1), rc(z_det_nom_kb, ndec = 1)))
-plt.plot(x1, yfit, 'r--', label = r'$I = {0}\times 10^{1}/\left({2} + \Delta d\right)^{{2}}$'.format(a_coeff, a_cap_exponent, round_correct(z_det_nom, ndec = 1)))
-# plt.plot(x1, yfit, 'r--', label = r'$I = {0}\times 10^{1}/\left({2} + \Delta d\right)^{{2}}$'.format(a_kb_coeff, a_kb_exponent, rc(z_det_nom_kb, ndec = 1)))
+plt.scatter(dz, i_tot_xrf, color = 'black')
+plt.plot(x1, yfit, 'r--', label = r'$I = {0}\times 10^{1}/\left({2} + \Delta d\right)^{{2}}$'.format(a_coeff, a_exponent, round_correct(z_det_nom, ndec = 1)))
 plt.xticks(fontsize = 14)
 plt.yticks(fontsize = 14)
 plt.tick_params('both', length = 9)
@@ -125,47 +103,3 @@ plt.legend(frameon = False, fontsize = 14)
 plt.tight_layout()
 # plt.savefig("/Users/bwr0835/Documents/GitHub/gradresearch/8_bm_res_exp/writeup/solid_angle_kb.svg")
 plt.show()
-
-
-# def i_obj_cap(dz_cap, a_cap, z_det_nom_cap):
-    # return a_cap/(z_det_nom_cap + dz_cap)**2
-
-# x_kb, y_kb = dz_kb, i_tot_kb
-# # x_cap, y_cap = dz_cap, i_tot_cap
-
-# fit_kb = cf(i_obj_kb, dz_kb, i_tot_kb)
-# # fit_cap = cf(i_obj_cap, dz_cap, i_tot_cap)
-
-# a_kb, z_det_nom_kb = fit_kb[0]
-# # a_cap, z_det_nom_cap = fit_cap[0]
-# print(a_kb)
-# print(z_det_nom_kb)
-
-# x = np.linspace(np.min(dz_kb), np.max(dz_kb), 1000)
-# y = i_obj_kb(x, a_kb, z_det_nom_kb)
-
-# # print(a_cap)
-# # print(z_det_nom_cap)
-
-# # print(np.shape(fe_0160))
-# # print(np.shape(fe_0161))
-
-# # x = np.linspace(np.min(dz_cap), np.max(dz_cap), 1000)
-# # y = i_obj_cap(x, a_cap, z_det_nom_cap)
-
-# # x = np.linspace(np.min(dz_cap), np.max(dz_cap), 1000)
-# # y = i_obj_cap(x, a_cap, z_det_nom_cap)
-
-# # plt.imshow(fe_0161 - fe_0160)
-
-# plt.plot(x, y, 'r--', label = r'$\tilde{{I}} = {0}/\left({1} + \Delta d\right)^{{2}}$'.format(rc(a_kb, ndec = 1), rc(z_det_nom_kb, ndec = 1)))
-# plt.scatter(dz_kb, i_tot_kb, color = 'black')
-# plt.xticks(fontsize = 14)
-# plt.yticks(fontsize = 14)
-# plt.tick_params('both', length = 9)
-# plt.xlabel(r'$\Delta d$ (mm)', fontsize = 16)
-# plt.ylabel(r'$\tilde{I}$ (a.u.)', fontsize = 16)
-# plt.legend(frameon = False, fontsize = 14)
-# plt.tight_layout()
-# # plt.savefig("/Users/bwr0835/Documents/GitHub/gradresearch/8_bm_res_exp/writeup/solid_angle_kb.svg")
-# plt.show()
