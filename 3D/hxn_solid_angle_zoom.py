92444ab81af6cf1f661ba105e1bcc9b92aee0788
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-22 00:01:04 -0500

    Update hxn_solid_angle_zoom.py

--- 3D/hxn_solid_angle_zoom.py
+++ 3D/hxn_solid_angle_zoom.py
@@ -24,11 +24,11 @@ x1_coarse_idx, y1_coarse_idx = np.argmin(np.abs(x_um_coarse - x1_fine)), np.argm
 x2_coarse_idx, y2_coarse_idx = np.argmin(np.abs(x_um_coarse - x2_fine)), np.argmin(np.abs(y_um_coarse - y2_fine))
 x3_coarse_idx, y3_coarse_idx = np.argmin(np.abs(x_um_coarse - x3_fine)), np.argmin(np.abs(y_um_coarse - y3_fine))
 
-fig, axs = plt.subplots(1, 2)
-
 print(x0_coarse_idx)
 
 sys.exit()
+
+fig, axs = plt.subplots(1, 2)
 
 pixel_width = x1_coarse_idx[1] - x0_coarse_idx[1]
 pixel_height = y1_coarse_idx[0] - y0_coarse_idx[0]
