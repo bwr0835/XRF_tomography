a7969ebc1a19dcdad9a8a7c5aa1bc2a2052d582d
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-22 13:50:23 -0500

    Update hxn_solid_angle_zoom.py

--- 3D/hxn_solid_angle_zoom.py
+++ 3D/hxn_solid_angle_zoom.py
@@ -7,7 +7,7 @@ plt.rcParams["figure.autolayout"] = True
 coarse_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235675.h5'
 fine_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235676.h5'
 
-elements, counts_coarse, _, x_um_coarse, y_um_coarse, nx_coarse, ny_coarse, dx_cm_coarse, dy_cm_coarse = util.extract_h5_xrf_data(coarse_scan_filename, synchrotron = 'nsls-ii', scan_coords = True)
+elements, counts_coarse, _,  _, x_um_coarse, y_um_coarse, nx_coarse, ny_coarse, dx_cm_coarse, dy_cm_coarse = util.extract_h5_xrf_data(coarse_scan_filename, synchrotron = 'nsls-ii', scan_coords = True, US_IC = True)
 _, counts_fine, us_ic, _, x_um_fine, y_um_fine, nx_fine, ny_fine, dx_cm_fine, dy_cm_fine = util.extract_h5_xrf_data(fine_scan_filename, synchrotron = 'nsls-ii', scan_coords = True, US_IC = True)
 
 desired_element = 'Fe_K'
