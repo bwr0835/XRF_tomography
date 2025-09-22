5f4afd0a8ca8de0a582e7be344b3f87b588872f2
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-22 00:33:36 -0500

    Update hxn_solid_angle_zoom.py

--- 3D/hxn_solid_angle_zoom.py
+++ 3D/hxn_solid_angle_zoom.py
@@ -6,7 +6,7 @@ coarse_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235675
 fine_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235676.h5'
 
 elements, counts_coarse, _, x_um_coarse, y_um_coarse, nx_coarse, ny_coarse, dx_cm_coarse, dy_cm_coarse = util.extract_h5_xrf_data(coarse_scan_filename, synchrotron = 'nsls-ii', scan_coords = True)
-_, counts_fine, _, x_um_fine, y_um_fine, nx_fine, ny_fine, dx_cm_fine, dy_cm_fine = util.extract_h5_xrf_data(coarse_scan_filename, synchrotron = 'nsls-ii', scan_coords = True)
+_, counts_fine, _, x_um_fine, y_um_fine, nx_fine, ny_fine, dx_cm_fine, dy_cm_fine = util.extract_h5_xrf_data(fine_scan_filename, synchrotron = 'nsls-ii', scan_coords = True)
 
 desired_element = 'Fe_K'
 desired_element_index = elements.index(desired_element)
