13babdbba3fad78541c05e5f2552bc3069f22ff5
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-21 23:56:20 -0500

    Update hxn_solid_angle_zoom.py

--- 3D/hxn_solid_angle_zoom.py
+++ 3D/hxn_solid_angle_zoom.py
@@ -1,6 +1,6 @@
 import numpy as np, h5py, h5_util as util
 
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, patches as pat
 
 coarse_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235675.h5'
 fine_scan_filename = '/raid/users/roter/Jacobsen-nslsii/data/xrf/scan2D_235676.h5'
@@ -9,4 +9,29 @@ elements, counts_coarse, _, x_um_coarse, y_um_coarse, nx_coarse, ny_coarse, dx_c
 _, counts_fine, _, x_um_fine, y_um_fine, nx_fine, ny_fine, dx_cm_fine, dy_cm_fine = util.extract_h5_xrf_data(coarse_scan_filename, synchrotron = 'nsls-ii', scan_coords = True)
 
 desired_element = 'Fe_K'
-desired_element_indx = elements.index(desired_element)
+desired_element_index = elements.index(desired_element)
+
+fe_coarse = counts_coarse[desired_element_index]
+fe_fine = counts_fine[desired_element_index]
+
+x0_fine, y0_fine = x_um_fine[0, 0], y_um_fine[0, 0] # Top left corner
+x1_fine, y1_fine = x_um_fine[0, -1], y_um_fine[0, -1] # Top right corner
+x2_fine, y2_fine = x_um_fine[-1, 0], y_um_fine[-1, 0] # Bottom left corner
+x3_fine, y3_fine = x_um_fine[-1, -1], y_um_fine[-1, -1] # Bottom right corner
+
+x0_coarse_idx, y0_coarse_idx = np.argmin(np.abs(x_um_coarse - x0_fine)), np.argmin(np.abs(y_um_coarse - y0_fine))
+x1_coarse_idx, y1_coarse_idx = np.argmin(np.abs(x_um_coarse - x1_fine)), np.argmin(np.abs(y_um_coarse - y1_fine))
+x2_coarse_idx, y2_coarse_idx = np.argmin(np.abs(x_um_coarse - x2_fine)), np.argmin(np.abs(y_um_coarse - y2_fine))
+x3_coarse_idx, y3_coarse_idx = np.argmin(np.abs(x_um_coarse - x3_fine)), np.argmin(np.abs(y_um_coarse - y3_fine))
+
+fig, axs = plt.subplots(1, 2)
+
+pixel_width = x1_coarse_idx[1] - x0_coarse_idx[1]
+pixel_height = y1_coarse_idx[0] - y0_coarse_idx[0]
+
+rect = pat.Rectangle(x0_coarse_idx[1] - 0.5, y0_coarse_idx[0] - 0.5, pixel_width, pixel_height, edge_color = 'white', facecolor = 'none')
+
+axs[0].imshow(fe_coarse)
+axs[1].imshow(fe_fine)
+
+plt.show()
