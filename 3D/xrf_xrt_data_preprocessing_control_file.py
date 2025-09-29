212638113192b0089d357d7409f85d94160dd023
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-09-28 23:49:15 -0500

    Update xrf_xrt_data_preprocessing_control_file.py

--- 3D/xrf_xrt_data_preprocessing_control_file.py
+++ 3D/xrf_xrt_data_preprocessing_control_file.py
@@ -16,10 +16,61 @@ def preprocess_xrf_xrt_data(synchrotron,
                             iterative_reproj_enabled):
         
     if create_aggregate_xrf_xrt_files_enabled:
-        root = tk.Tk()
+        xrf_file_array = ['/raid/users/roter/Jacobsen/img.dat/2xfm_0030.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0031.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0032.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0033.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0034.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0117.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0118.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0037.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0038.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0039.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0040.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0047.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0042.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0048.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0049.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0050.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0051.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0052.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0053.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0054.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0055.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0056.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0057.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0058.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0059.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0060.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0094.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0095.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0096.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0097.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0098.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0099.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0100.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0101.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0102.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0103.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0104.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0105.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0106.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0107.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0108.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0109.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0110.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0111.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0112.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0113.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0114.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0115.mda.h5',
+                          '/raid/users/roter/Jacobsen/img.dat/2xfm_0116.mda.h5']
+        
+        xrt_file_array = xrf_file_array
+        # root = tk.Tk()
 
-        xrf_file_array = fd.askopenfilenames(parent = root, title = "Choose XRF files to aggregate.", filetypes = [('HDF5 files', '*.h5')])
-        xrt_file_array = fd.askopenfilenames(parent = root, title = "Choose XRT files to aggregate.", filetypes = [('HDF5 files', '*.h5')])
+        # xrf_file_array = fd.askopenfilenames(parent = root, title = "Choose XRF files to aggregate.", filetypes = [('HDF5 files', '*.h5')])
+        # xrt_file_array = fd.askopenfilenames(parent = root, title = "Choose XRT files to aggregate.", filetypes = [('HDF5 files', '*.h5')])
 
         if xrf_file_array == '' or xrt_file_array == '':
             print('Error: XRF and/or XRT filename array empty. Exiting program...')
