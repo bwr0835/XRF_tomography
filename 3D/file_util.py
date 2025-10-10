659082034cb6a9659c0f96afa452b7c30d062d20
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-10-10 16:01:40 -0500

    Update file_util.py

--- 3D/file_util.py
+++ 3D/file_util.py
@@ -641,7 +641,7 @@ def extract_csv_preprocessing_input_params(file_path):
                                    names = ['input_param', 'value'],
                                    dtype = str,
                                    keep_default_na = False,
-                                   index_col = False)
+                                   index = False)
 
     try:
         input_params = input_params_csv['input_param']
