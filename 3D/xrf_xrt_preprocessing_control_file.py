d4282be7d45f799998e0625c7a8c12bf97a9ef30
bwr0835 <benjaminroter2026@u.northwestern.edu>
2025-10-12 13:36:24 -0500

    Update xrf_xrt_preprocessing_control_file.py

--- 3D/xrf_xrt_preprocessing_control_file.py
+++ 3D/xrf_xrt_preprocessing_control_file.py
@@ -29,9 +29,8 @@ def preprocess_xrf_xrt_data(synchrotron,
                             eps,
                             aligned_data_output_dir_path):
 
-    print('OK')
-
     if create_aggregate_xrf_xrt_files_enabled:
+        print('OK')
         if pre_existing_aggregate_xrf_xrt_file_lists_enabled:
             print('Extracting pre-existing XRF, XRT HDF5 file lists...')
             
