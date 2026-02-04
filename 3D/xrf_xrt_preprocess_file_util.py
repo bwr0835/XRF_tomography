3e87ed2bf02ea9310b7f8ab898da59ce476161aa
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-04 14:51:04 -0600

    Update xrf_xrt_preprocess_file_util.py

--- 3D/xrf_xrt_preprocess_file_util.py
+++ 3D/xrf_xrt_preprocess_file_util.py
@@ -725,22 +725,22 @@ def extract_csv_preprocessing_input_params(file_path):
     missing_data = set(all_params_ordered) - set(input_params)
     extra_data = set(input_params) - set(all_params_ordered)
     
-    if not missing_data or not extra_data:
-        if not bool(missing_data) and not bool(extra_data):
-            print('Error: The following input parameters are missing:\n')
-            print(*(["'{}'".format(s) for s in missing_data]), sep = '\n')
-            print('\nAdditionally, the following input parameters should be removed:\n')
-            print(*(["'{}'".format(s) for s in extra_data]), sep = '\n')
+    # if not missing_data or not extra_data:
+    #     if not bool(missing_data) and not bool(extra_data):
+    #         print('Error: The following input parameters are missing:\n')
+    #         print(*(["'{}'".format(s) for s in missing_data]), sep = '\n')
+    #         print('\nAdditionally, the following input parameters should be removed:\n')
+    #         print(*(["'{}'".format(s) for s in extra_data]), sep = '\n')
 
-        elif not bool(missing_data):
-            print('Error: The following input parameters are missing:\n')
-            print(*(["'{}'".format(s) for s in missing_data]), sep = '\n')
+    #     elif not bool(missing_data):
+    #         print('Error: The following input parameters are missing:\n')
+    #         print(*(["'{}'".format(s) for s in missing_data]), sep = '\n')
 
-        else:
-            print('Error: The following input parameters should be removed:\n')
-            print(*(["'{}'".format(s) for s in extra_data]), sep = '\n')
+    #     else:
+    #         print('Error: The following input parameters should be removed:\n')
+    #         print(*(["'{}'".format(s) for s in extra_data]), sep = '\n')
 
-        sys.exit()
+    #     sys.exit()
 
     for idx, val in enumerate(values): # Convert strings supposed to be numberic or Boolean to floats, ints, or bools
         if val.lower() == 'none':
