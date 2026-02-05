df23662270467a875750f773b8db79206b41f55e
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-04 19:52:32 -0600

    Update h5_aggregate_edit.py

--- 3D/h5_aggregate_edit.py
+++ 3D/h5_aggregate_edit.py
@@ -4,11 +4,11 @@ file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregat
 
 output_file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt.h5'
 
-with h5py.File(file_path, 'r+') as f:
-    theta = f['exchange/theta']
+# with h5py.File(file_path, 'r+') as f:
+#     theta = f['exchange/theta']
 
-    theta[35] = -69
-    theta[53] = -15
+#     theta[35] = -69
+#     theta[53] = -15
 
 with h5py.File(file_path, 'r') as f:
     theta = f['exchange/theta'][()]
