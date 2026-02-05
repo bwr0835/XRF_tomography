8bf20c9a0dd6e556c1e79468cc4a90911e4e26d1
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-04 19:08:02 -0600

    Update h5_aggregate_edit.py

--- 3D/h5_aggregate_edit.py
+++ 3D/h5_aggregate_edit.py
@@ -3,7 +3,7 @@ import numpy as np, h5py
 file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt_orig.h5'
 
 with h5py.File(file_path, 'r+') as f:
-    theta = f['exchange/theta'][()]
+    theta = f['exchange/theta']
 
     theta[35] = -69
     theta[53] = -15
