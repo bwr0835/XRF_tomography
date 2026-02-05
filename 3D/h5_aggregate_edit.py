88237fada8ece1d9da8dc8813517efb68b79399c
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-04 19:43:57 -0600

    Update h5_aggregate_edit.py

--- 3D/h5_aggregate_edit.py
+++ 3D/h5_aggregate_edit.py
@@ -4,38 +4,38 @@ file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregat
 
 output_file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt.h5'
 
-# with h5py.File(file_path, 'r+') as f:
-#     theta = f['exchange/theta']
+with h5py.File(file_path, 'r+') as f:
+    theta = f['exchange/theta']
 
-#     theta[35] = -69
-#     theta[53] = -15
+    theta[35] = -72
+    theta[53] = -18
 
-with h5py.File(file_path, 'r') as f:
-    theta = f['exchange/theta'][()]
-    filenames = f['filenames'][()]
-    data = f['exchange/data'][()]
-    elements = f['exchange/elements'][()]
+# with h5py.File(file_path, 'r') as f:
+#     theta = f['exchange/theta'][()]
+#     filenames = f['filenames'][()]
+#     data = f['exchange/data'][()]
+#     elements = f['exchange/elements'][()]
 
-idx = np.r_[:35, 36:53, 54:]
+# idx = np.r_[:35, 36:53, 54:]
 
-theta_idx_sorted = np.argsort(theta)
+# theta_idx_sorted = np.argsort(theta)
 
-theta_sorted = theta[theta_idx_sorted]
-filenames_sorted = filenames[theta_idx_sorted]
-data_sorted = data[:, theta_idx_sorted]
+# theta_sorted = theta[theta_idx_sorted]
+# filenames_sorted = filenames[theta_idx_sorted]
+# data_sorted = data[:, theta_idx_sorted]
 
-print(elements)
+# print(elements)
 
-with h5py.File(output_file_path, 'w') as f:
-    exchange = f.create_group('exchange')
+# with h5py.File(output_file_path, 'w') as f:
+#     exchange = f.create_group('exchange')
     
-    intensity = exchange.create_dataset('data', data = data_sorted)
-    el = exchange.create_dataset('elements', data = elements)
-    angle = exchange.create_dataset('theta', data = theta_sorted)
+#     intensity = exchange.create_dataset('data', data = data_sorted)
+#     el = exchange.create_dataset('elements', data = elements)
+#     angle = exchange.create_dataset('theta', data = theta_sorted)
     
-    fnames = f.create_dataset('filenames', data = filenames_sorted)
+#     fnames = f.create_dataset('filenames', data = filenames_sorted)
 
-    intensity.attrs['dataset_type'] = 'xrt'
-    intensity.attrs['us_ic_scaler_name'] = 'sclr1_ch4'
-    intensity.attrs['xrt_signal_name'] = 'stxm'
+#     intensity.attrs['dataset_type'] = 'xrt'
+#     intensity.attrs['us_ic_scaler_name'] = 'sclr1_ch4'
+#     intensity.attrs['xrt_signal_name'] = 'stxm'
     
