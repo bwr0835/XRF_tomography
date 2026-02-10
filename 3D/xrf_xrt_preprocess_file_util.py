56a448f1399c93d3c3e104ec767f58fbf803bd1a
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-09 19:29:10 -0600

    Update xrf_xrt_preprocess_file_util.py

--- 3D/xrf_xrt_preprocess_file_util.py
+++ 3D/xrf_xrt_preprocess_file_util.py
@@ -1204,9 +1204,9 @@ def create_nonaligned_norm_non_cropped_proj_data_gif(dir_path,
 
         fig3, axs3 = plt.subplots(1, 3)
 
-        im3_1 = axs3[0].imshow(counts_xrt_norm[:, 0], vmin = vmin_xrt_norm, vmax = vmax_xrt_norm, extent = [0, n_slices - 1, -180, 180], aspect = 20)
-        im3_2 = axs3[1].imshow(opt_dens[:, 0], vmin = vmin_opt_dens, vmax = vmax_opt_dens, extent = [0, n_slices - 1, -180, 180], aspect = 20)
-        im3_3 = axs3[2].imshow(counts_xrf_ref_element_norm[:, 0], vmin = vmin_xrf_norm, extent = [0, n_slices - 1, -180, 180], vmax = vmax_xrf_norm, aspect = 20)
+        im3_1 = axs3[0].imshow(counts_xrt_norm[:, 0], vmin = vmin_xrt_norm, vmax = vmax_xrt_norm, extent = [0, n_slices - 1, -180, 180], aspect = 1.5)
+        im3_2 = axs3[1].imshow(opt_dens[:, 0], vmin = vmin_opt_dens, vmax = vmax_opt_dens, extent = [0, n_slices - 1, -180, 180], aspect = 1.5)
+        im3_3 = axs3[2].imshow(counts_xrf_ref_element_norm[:, 0], vmin = vmin_xrf_norm, extent = [0, n_slices - 1, -180, 180], vmax = vmax_xrf_norm, aspect = 1.5)
 
         text_3 = axs3[0].text(0.02, 0.02, r'Slice index 0/{0}'.format(n_slices - 1), transform = axs3[0].transAxes, color = 'white')
         
