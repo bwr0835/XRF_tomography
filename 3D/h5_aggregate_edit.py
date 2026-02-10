84360d5c74870615a358b76ff403a1d238c6b462
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-09 20:42:43 -0600

    Update h5_aggregate_edit.py

--- 3D/h5_aggregate_edit.py
+++ 3D/h5_aggregate_edit.py
@@ -1,52 +1,51 @@
 import numpy as np, h5py
 
 # file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt_orig.h5'
-# file_path = '/raid/users/roter/Jacobsen-nslsii/data/xrf/3_id_aggregate_xrf_orig.h5'
-file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt.h5'
+file_path = '/raid/users/roter/Jacobsen-nslsii/data/xrf/3_id_aggregate_xrf_orig.h5'
+# file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt.h5'
 
 # output_file_path = '/raid/users/roter/Jacobsen-nslsii/data/ptycho/h5_data/3_id_aggregate_xrt.h5'
-# output_file_path = '/raid/users/roter/Jacobsen-nslsii/data/xrf/3_id_aggregate_xrf.h5'
+output_file_path = '/raid/users/roter/Jacobsen-nslsii/data/xrf/3_id_aggregate_xrf.h5'
 
-with h5py.File(file_path, 'r+') as f:
-    data_sorted = f['exchange/data'][()]
+# with h5py.File(file_path, 'r+') as f:
+#     theta = f['exchange/theta'][()]
+#     data_sorted = f['exchange/data'][()]
     # f['exchange/data'].attrs['dataset_type'] = 'xrt'
 
-# with h5py.File(file_path, 'r') as f:
-#     theta = f['exchange/theta'][()]
-#     filenames = f['filenames'][()]
-#     data = f['exchange/data'][()]
-#     elements = f['exchange/elements'][()]
+with h5py.File(file_path, 'r') as f:
+    theta = f['exchange/theta'][()]
+    filenames = f['filenames'][()]
+    data = f['exchange/data'][()]
+    elements = f['exchange/elements'][()]
 
-# theta_idx_sorted = np.argsort(theta)
+theta_idx_sorted = np.argsort(theta)
 
-# n_slices, n_columns = data.shape[2:]
+n_slices, n_columns = data.shape[2:]
 
-# keep = (theta_idx_sorted != 35) & (theta_idx_sorted != 53)
-# theta_sorted = theta[theta_idx_sorted][keep]
-# data_sorted = data[:, theta_idx_sorted][:, keep]
+keep = (theta_idx_sorted != 35) & (theta_idx_sorted != 53)
+theta_sorted = theta[theta_idx_sorted][keep]
+data_sorted = data[:, theta_idx_sorted][:, keep]
 
-# filenames_sorted = [filenames[i] for i in theta_idx_sorted if i != 35 and i != 53]
-
-    data_sorted[:, 56:] = np.flip(data_sorted[:, 56:], axis = (2, 3))
+filenames_sorted = [filenames[i] for i in theta_idx_sorted if i != 35 and i != 53]
 
 # # print(filenames_sorted)
 
-# with h5py.File(output_file_path, 'w') as f:
-#     exchange = f.create_group('exchange')
+with h5py.File(output_file_path, 'w') as f:
+    exchange = f.create_group('exchange')
     
-#     intensity = exchange.create_dataset('data', data = data_sorted, compression = 'gzip')
-#     el = exchange.create_dataset('elements', data = elements)
-#     angle = exchange.create_dataset('theta', data = theta_sorted)
-    
-#     fnames = f.create_dataset('filenames', data = filenames_sorted)
+    intensity = exchange.create_dataset('data', data = data_sorted, compression = 'gzip')
+    el = exchange.create_dataset('elements', data = elements)
+    angle = exchange.create_dataset('theta', data = theta_sorted)
+    # 
+    fnames = f.create_dataset('filenames', data = filenames_sorted)
 
 #     # intensity.attrs['dataset_type'] = 'xrt'
 #     # intensity.attrs['us_ic_scaler_name'] = 'sclr1_ch4'
 #     # intensity.attrs['xrt_signal_name'] = 'stxm'
-#     exchange.attrs['raw_spectrum_fitting_method'] = 'NNLS'
-#     exchange.attrs['raw_spectrum_fitting_software'] = 'PyMCA'
+    exchange.attrs['raw_spectrum_fitting_method'] = 'NNLS'
+    exchange.attrs['raw_spectrum_fitting_software'] = 'PyMCA'
 
-#     intensity.attrs['dataset_type'] = 'xrf'
+    intensity.attrs['dataset_type'] = 'xrf'
 
 # with h5py.File(file_path, 'r+') as f:
 #     # Read theta into memory so comparisons (e.g. == 0) work correctly;
