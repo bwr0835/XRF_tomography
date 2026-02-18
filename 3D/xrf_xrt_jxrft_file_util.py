c2d27346890fdda966f383faf63ad861256e452a
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-02-18 12:41:17 -0600

    Update xrf_xrt_jxrft_file_util.py

--- 3D/xrf_xrt_jxrft_file_util.py
+++ 3D/xrf_xrt_jxrft_file_util.py
@@ -54,6 +54,8 @@ def extract_csv_input_jxrft_recon_params(file_path, fluor_lines, dev):
             
         print('\n\rEnding program...', flush = True)
 
+        comm.abort(1)
+
     # available_synchrotrons = ipn.available_synchrotrons
     available_noise_models = ipn.available_noise_models
     numeric_scalar_params = ipn.recon_numeric_scalar_params
