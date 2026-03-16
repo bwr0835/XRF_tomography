47512bf393b6bd20fa82bc91989d0f319bbee2c1
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-03-16 15:19:47 -0500

    Update test.py

--- 3D/test.py
+++ 3D/test.py
@@ -199,7 +199,8 @@ for theta_idx in range(n_theta):
 
 mask_avg_tot /= n_theta
 
-xrt_sig *= (mask_avg_tot)
+xrt_sig *= mask_avg_tot
+xrt_sig_copy = xrt_sig.copy()
 
 for theta_idx in range(n_theta):
     xrt_vignetted = ppu.edge_gauss_filter(xrt_sig[theta_idx], sigma = 5, alpha = 10, nx = n_columns, ny = n_slices)
@@ -208,7 +209,7 @@ for theta_idx in range(n_theta):
 
     threshold = np.percentile(convolution_mag, 80) # Take top 20% of intensities for masking
 
-    mask_avg[theta_idx] = xrt_sig[theta_idx, mask].mean()/mask_avg_tot
+    mask_avg[theta_idx] = xrt_sig[theta_idx, mask].mean()
 
 fig, axs = plt.subplots()
 plt.plot(theta_xrt, mask_avg)
