49eac4e670b81e0736e6b7167c0a270e17cfad18
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-03-30 14:00:39 -0500

    Update tomo_movie.py

--- 3D/tomo_movie.py
+++ 3D/tomo_movie.py
@@ -1,308 +1,62 @@
-import numpy as np, tkinter as tk, os, re
+import numpy as np, os, sys, xrf_xrt_preprocess_file_util as futil, xrf_xrt_preprocess_utils as ppu
 
-from matplotlib import pyplot as plt, animation as anim
-from tkinter import filedialog
-from numpy.fft import fftshift
+from matplotlib import pyplot as plt
+from scipy import ndimage as ndi
 
 plt.rcParams['text.usetex'] = True
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['text.latex.preamble'] = r'\usepackage{times}'
 
-# def load_dir(dir_path, filter_function = None):
-#     subdir_array = [subdir for subdir in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, subdir))]
+def create_cor_fig(init_proj, shifted_proj, theta_pair, vmin, vmax):
+    fig, axs = plt.subplots(2, 3)
 
-#     if filter_function:
-#         subdir_array_new = [subdir for subdir in subdir_array if filter_function(subdir)]
+    init_proj_theta_0 = init_proj[theta_pair[0]]
+    init_proj_theta_1 = init_proj[theta_pair[1]]
+    shifted_proj_theta_0 = shifted_proj[theta_pair[0]]
+    shifted_proj_theta_1 = shifted_proj[theta_pair[1]]
 
-#         return subdir_array_new
     
-#     else:
-#         return subdir_array
-
-def load_dir(dir_path):
-    subdir_array = [subdir for subdir in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, subdir))]
-    
-    theta_array_file_path = os.path.join(dir_path, 'theta_array.npy')
-
-    theta_array = np.load(theta_array_file_path)
-    theta_array = np.sort(theta_array)
-
-    return subdir_array, theta_array
-
-def get_theta(file_name):
-    return int(file_name.split('_')[2].strip())
-
-def get_slice(file_name):
-    return int(file_name.split('_')[2].split('.')[0])
-
-def get_iteration(subdir_name):
-    return int(subdir_name.split('_')[1])
-
-def normalize_array(array):
-    return (array - np.nanmin(array))/(np.nanmax(array) - np.nanmin(array))
-# def get_num_status(file_name):
-#     return bool(re.search(r'\d', file_name))
-
-root = tk.Tk()
-    
-root.withdraw()
-
-# directory_path = filedialog.askdirectory(parent = root, title = "Select directory", filetypes = (('NPY Files', '*.npy')))
-
-directory_path = '/raid/users/roter/gridrec'
-
-if directory_path == '':
-    print('Program aborted.')
-
-else:
-    n_theta = 49
-    n_slices = 301
-    n_columns = 599
-
-    recon = np.zeros((n_slices, n_columns, n_columns))
-    synthetic_proj = np.zeros((n_theta, n_slices, n_columns))
-    actual_proj = np.zeros((n_theta, n_slices, n_columns))
-    xcorr_proj = np.zeros((n_theta, n_slices, n_columns))
-
-    file_path_idx = 0
-
-    # subdir_array = load_dir(directory_path, lambda subdir: subdir.startswith('iteration_'))
-    subdir_array, theta_array = load_dir(directory_path)
-    
-    iteration_subdir_array = []
-    
-    synthetic_proj_data_dict = {}
-    actual_proj_data_dict = {}
-    xcorr_proj_data_dict = {}
-    recon_data_dict = {}
-    shift_change_dict = {}
-    
-    for subdir in subdir_array:
-        if subdir != 'net_shifts':
-            iteration_subdir_array.append(subdir)
-        
-        else:
-            net_shift_subdir = subdir
-    
-    iteration_subdir_array = sorted(iteration_subdir_array, key = get_iteration)
-
-    iteration_subdir_array_truncated = [iteration_subdir_array[0], iteration_subdir_array[-1]]
-
-    n_iterations = len(iteration_subdir_array)    
-
-    for idx, subdir in enumerate(iteration_subdir_array_truncated):
-        synthetic_proj_data = []
-        actual_proj_data = []
-        xcorr_proj_data = []
-        recon_data = []
-        shift_rgb_data = []
-
-        synthetic_proj_dir_path = os.path.join(directory_path, subdir, 'synthesized')
-        actual_proj_dir_path = os.path.join(directory_path, subdir, 'experimental')
-        xcorr_dir_path = os.path.join(directory_path, subdir, 'xcorr')
-        recon_dir_path = os.path.join(directory_path, subdir, 'recon')
-
-        proj_file_name = [file_name for file_name in os.listdir(synthetic_proj_dir_path)]
-        # actual_proj_file_path = [file_name for file_name in os.listdir(actual_proj_dir_path)]
-        # xcorr_proj_file_path = [file_name for file_name in os.listdir(xcorr_dir_path)]
-        recon_file_name = [file_name for file_name in os.listdir(recon_dir_path)]
-
-        proj_file_name = sorted(proj_file_name, key = get_theta)
-        recon_file_name = sorted(recon_file_name, key = get_slice)
-
-        synthetic_proj_file_path = [os.path.join(synthetic_proj_dir_path, file_name) for file_name in proj_file_name]
-        actual_proj_file_path = [os.path.join(actual_proj_dir_path, file_name) for file_name in proj_file_name]
-        xcorr_proj_file_path = [os.path.join(xcorr_dir_path, file_name) for file_name in proj_file_name]
-        recon_file_path = [os.path.join(recon_dir_path, file_name) for file_name in recon_file_name]
-
-        print('Loading projection images...')
-
-        for theta_idx in range(n_theta):
-            synthetic_proj[theta_idx] = np.load(synthetic_proj_file_path[theta_idx])
-            actual_proj[theta_idx] = np.load(actual_proj_file_path[theta_idx])
-            xcorr_proj[theta_idx] = fftshift(np.load(xcorr_proj_file_path[theta_idx]))
-
-            synthetic_proj_scaled = normalize_array(synthetic_proj[theta_idx])
-            actual_proj_scaled = normalize_array(actual_proj[theta_idx])
-
-            shift_rgb = np.dstack((actual_proj_scaled, np.zeros_like(synthetic_proj_scaled), synthetic_proj_scaled))
-
-            synthetic_proj_data.append(synthetic_proj[theta_idx])
-            actual_proj_data.append(actual_proj[theta_idx])
-            xcorr_proj_data.append(xcorr_proj[theta_idx])
-            shift_rgb_data.append(shift_rgb)
-
-        print('Loading reconstructions...')
-
-        for slice_idx in range(n_slices):
-            recon[slice_idx] = np.load(recon_file_path[slice_idx])
-
-            recon_data.append(recon[slice_idx])
-
-        synthetic_proj_data = np.array(synthetic_proj_data)
-        actual_proj_data = np.array(actual_proj_data)
-        xcorr_proj_data = np.array(xcorr_proj_data)
-        recon_data = np.array(recon_data)
-        shift_rgb_data = np.array(shift_rgb_data)
-        
-        synthetic_proj_data_dict[subdir] = synthetic_proj_data
-        actual_proj_data_dict[subdir] = actual_proj_data
-        xcorr_proj_data_dict[subdir] = xcorr_proj_data
-        recon_data_dict[subdir] = recon_data
-        shift_change_dict[subdir] = shift_rgb_data
-
-    x_shifts_file_path = os.path.join(directory_path, net_shift_subdir, 'x_shift_array.npy')
-    y_shifts_file_path = os.path.join(directory_path, net_shift_subdir, 'y_shift_array.npy')
-
-    x_shifts_data = np.load(x_shifts_file_path)
-    y_shifts_data = np.load(y_shifts_file_path)
-
-    # plt.plot(theta_array, x_shifts_data[0, :], 'k-o', label = r'Iteration Index 0')
-    # plt.plot(theta_array, x_shifts_data[-1, :], 'bo-', label = r'Iteration Index 9')
-    # plt.legend()
-    
-    fig1, axs1 = plt.subplots(2, 1)
-    fig2, axs2 = plt.subplots(2, 4)
-    fig3, axs3 = plt.subplots()
-
-    iter_array = 1 + np.arange(n_iterations)
-
-    curve1, = axs3.plot(iter_array, np.zeros_like(iter_array), 'k-o', markersize = 3, label = r'$\Delta x$')
-    curve2, = axs3.plot(iter_array, np.zeros_like(iter_array), 'r-o', markersize = 3, label = r'$\Delta y$')
-
-    axs1[0].set_title(r'Recon. Slice (It. 1)')
-    axs1[1].set_title(r'Recon. Slice (It. {0})'.format(n_iterations))
-    axs2[0, 0].set_title(r'(Al.) Exp. Proj. (It. 1)')
-    axs2[1, 0].set_title(r'(Al.) Exp. Proj. (It. {0})'.format(n_iterations))
-    axs2[0, 1].set_title(r'Synth. Proj. (It. 1)')
-    axs2[1, 1].set_title(r'Synth. Proj. (It. {0})'.format(n_iterations))
-    axs2[0, 2].set_title(r'Int.-Pix. CC (It. 1)')
-    axs2[1, 2].set_title(r'Int.-Pix. CC (It. {0})'.format(n_iterations))
-    axs2[0, 3].set_title(r'Curr. shift (It. 1)')
-    axs2[1, 3].set_title(r'Curr. shift (It. {0})'.format(n_iterations))
-
-    axs3.set_xlabel(r'Iteration')
-    axs3.set_ylabel(r'Net shift (pixels)')
-    axs3.set_xlim(1, n_iterations)
-    axs3.legend(frameon = False)
-
-    recon_imgs = []
-    exp_proj_imgs = []
-    synthetic_proj_imgs = []
-    xcorr_imgs = []
-    shift_rgb_imgs = []
-    
-    recon_text = []
-    proj_text = []
-
-    text_recon = axs1[0].text(0.02, 0.02, r'Slice 0', transform = axs1[0].transAxes, color = 'white')
-
-    for idx, subdir in enumerate(iteration_subdir_array_truncated):
-        recons = recon_data_dict[subdir]
-        exp_projs = actual_proj_data_dict[subdir]
-        synth_projs = synthetic_proj_data_dict[subdir]
-        xcorrs = xcorr_proj_data_dict[subdir]
-        shift_rgbs = shift_change_dict[subdir]
-
-        im1 = axs1[idx].imshow(recons[0])
-        im2_0 = axs2[idx, 0].imshow(exp_projs[0])
-        im2_1 = axs2[idx, 1].imshow(synth_projs[0])
-        im2_2 = axs2[idx, 2].imshow(xcorrs[0])
-        im2_3 = axs2[idx, 3].imshow(shift_rgbs[0])
-
-        recon_imgs.append(im1)
-        exp_proj_imgs.append(im2_0)
-        synthetic_proj_imgs.append(im2_1)
-        xcorr_imgs.append(im2_2)
-        shift_rgb_imgs.append(im2_3)
-    
-        # text_recon = axs1[0].text(0.02, 0.02, r'Slice 0', transform = axs1[0].transAxes, color = 'white')
-        text_proj = axs2[0, 0].text(0.02, 0.02, r'$\theta = {0}$\textdegree'.format(theta_array[0]), transform = axs2[0, 0].transAxes, color = 'white')    
-        
-        if idx == 0:
-            # recon_text.append(text_recon)
-            proj_text.append(text_proj)
-
-    def animate_recon(frame):
-        artists = []
-
-        for idx, subdir in enumerate(iteration_subdir_array_truncated):
-            recons = recon_data_dict[subdir]
-
-            recon_imgs[idx].set_array(recons[frame])
-
-            artists.append(recon_imgs[idx])
-        
-        # recon_text.set_text(r'Slice {0}'.format(frame))
-        text_recon.set_text(r'Slice {0}'.format(frame))
-        
-        artists.append(text_recon)
-
-        return artists
-    
-    def animate_proj(frame):
-        artists = []
-
-        for idx, subdir in enumerate(iteration_subdir_array_truncated):
-            exp_projs = actual_proj_data_dict[subdir]
-            synth_projs = synthetic_proj_data_dict[subdir]
-            xcorrs = xcorr_proj_data_dict[subdir]
-            shift_rgbs = shift_change_dict[subdir]
-
-            exp_proj_imgs[idx].set_array(exp_projs[frame])
-            synthetic_proj_imgs[idx].set_array(synth_projs[frame])
-            xcorr_imgs[idx].set_array(xcorrs[frame])
-            shift_rgb_imgs[idx].set_array(shift_rgbs[frame])
-
-            artists.append(exp_proj_imgs[idx])
-            artists.append(synthetic_proj_imgs[idx])
-            artists.append(xcorr_imgs[idx])
-            artists.append(shift_rgb_imgs[idx])
-        
-        proj_text[0].set_text(r'$\theta = {0}$\textdegree'.format(theta_array[frame]))
-
-        artists.append(proj_text[0])
-
-        return artists
-    
-    fps_shifts = 10 # Frames per second (fps)
-    fps_images = 25
-
-    def animate_shifts(frame):
-        artists = []
-        
-        net_shift_x = x_shifts_data[:, frame]
-        net_shift_y = y_shifts_data[:, frame]
-
-        curve1.set_ydata(net_shift_x)
-        curve2.set_ydata(net_shift_y)
-
-        min_shift = np.min([np.min(net_shift_x), np.min(net_shift_y)])
-        max_shift = np.max([np.max(net_shift_x), np.max(net_shift_y)])
-
-        axs3.set_ylim(min_shift, max_shift + 0.1)
-        axs3.set_title(r'$\theta = {0}$\textdegree'.format(theta_array[frame]))
-
-        artists.append(curve1)
-        artists.append(curve2)
-
-        return artists
-
-    anim1 = anim.FuncAnimation(fig1, animate_recon, frames = n_slices, interval = 1000/fps_images, blit = True) # Interval is ms/frame (NOT frames per second, or fps)
-    # anim2 = anim.FuncAnimation(fig2, animate_proj, frames = n_theta, interval = 1000/fps_images, blit = True)
-    # anim3 = anim.FuncAnimation(fig3, animate_shifts, frames = n_theta, interval = 1000/fps_shifts, blit = False) # "Blitting" not useful since titles are changing
-    
-    output_path1 = '/home/bwr0835/recon_gridrec_no_cor_correction.mp4'
-    writer1 = anim.FFMpegWriter(fps = fps_images, metadata = {'title': 'recon'}, bitrate = 3500, extra_args = ['-vcodec', 'libx264'])
-    
-    print('Saving')
-    
-    anim1.save(output_path1, writer1, dpi = 600)
-
-    # plt.show()
-
-
-
-        
+
+
+
+dir_path = '/Users/bwr0835/Documents'
+
+init_aps_xrt_file_path = f'{dir_path}/2_ide_aggregate_xrt.h5'
+init_hxn_xrt_file_path = f'{dir_path}/3_id_aggregate_xrt.h5'
+
+init_aps_xrf_file_path = f'{dir_path}/2_ide_aggregate_xrf.h5'
+init_hxn_xrf_file_path = f'{dir_path}/3_id_aggregate_xrf.h5'
+
+elements_xrt_aps, intensity_xrt_aps, theta_xrt_aps, _, _, _, _ = futil.extract_h5_aggregate_xrt_data(init_aps_xrt_file_path)
+elements_xrt_hxn, intensity_xrt_hxn, theta_xrt_hxn, _, _, _, _ = futil.extract_h5_aggregate_xrt_data(init_hxn_xrt_file_path)
+
+elements_xrf_aps, intensity_xrf_aps, theta_xrf_aps, _, _, _, _ = futil.extract_h5_aggregate_xrf_data(init_aps_xrf_file_path)
+elements_xrf_hxn, intensity_xrf_hxn, theta_xrf_hxn, _, _, _, _ = futil.extract_h5_aggregate_xrf_data(init_hxn_xrf_file_path)
+
+xrt_sig_aps = intensity_xrt_aps[elements_xrt_aps.index('xrt_sig')]
+
+aligning_element_aps = 'xrt_sig'
+aligning_element_hxn = 'Ni'
+
+aligning_element_idx_aps = elements_xrt_aps.index(aligning_element_aps)
+aligning_element_idx_hxn = elements_xrf_hxn.index(aligning_element_hxn)
+
+intensity_xrt_norm_aps, intensity_xrf_norm_aps, norm_array_xrt_aps, norm_array_xrf_aps, I0_photons_aps = ppu.joint_fluct_norm(xrt_sig_aps,
+                                                                                                                             intensity_xrf_aps,
+                                                                                                                             93,
+                                                                                                                             False,
+                                                                                                                             8.6776e8,
+                                                                                                                             0.01)
+theta_pair_aps = (np.where(theta_xrt_aps == -122)[0][0], np.where(theta_xrt_aps == 58)[0][0])
+
+opt_dens_aps = -np.log(intensity_xrt_norm_aps/I0_photons_aps)
+
+cor_shift_aps = -6.68587804108239
+
+shifted_opt_dens_aps = ndi.shift(opt_dens_aps, shift = (0, 0, cor_shift_aps))
+
+vmin_od = np.nanmin(opt_dens_aps, shifted_opt_dens_aps)
+vmax_od = np.nanmax(opt_dens_aps, shifted_opt_dens_aps)
+
 
 
