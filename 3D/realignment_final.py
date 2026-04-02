e8f7aebf6b39119b04a56ff8fc72867d7352ac1d
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-04-02 17:28:41 -0500

    Update realignment_final.py

--- 3D/realignment_final.py
+++ 3D/realignment_final.py
@@ -621,13 +621,13 @@ def realign_proj(cor_correction_only,
             shifts_init_first_part, _, _ = phase_xcorr_manual(aligned_proj[0], np.fliplr(aligned_proj[zero_deg_idx_array[0]]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([-180, 0]))
             shifts_init_second_part, _, _ = phase_xcorr_manual(aligned_proj[zero_deg_idx_array[1]], np.fliplr(aligned_proj[-1]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([0, 180]))
             
-            center_geom = aligned_proj.shape[1]//2
+            center_geom = aligned_proj.shape[2]//2
 
-            offset_init_first_part = shifts_init_first_part[1]/2
-            offset_init_second_part = shifts_init_second_part[1]/2
+            offset_init_first_part = -shifts_init_first_part[1]/2
+            offset_init_second_part = -shifts_init_second_part[1]/2
 
-            center_of_rotation_avg_first_part = center_geom + offset_init_first_part
-            center_of_rotation_avg_second_part = center_geom + offset_init_second_part
+            center_of_rotation_avg_first_part = center_geom - offset_init_first_part
+            center_of_rotation_avg_second_part = center_geom - offset_init_second_part
 
             print(f'Average center of rotation (before flipping sample): {ppu.round_correct(center_of_rotation_avg_first_part, ndec = 13)}')
             print(f'Average center of rotation (after flipping sample): {ppu.round_correct(center_of_rotation_avg_second_part, ndec = 13)}\n')
