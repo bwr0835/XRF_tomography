53f0d6c6035e928ca7a82b18f242bbffd9f589c7
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-04-03 13:31:22 -0500

    BR

--- 3D/realignment_final.py
+++ 3D/realignment_final.py
@@ -618,8 +618,8 @@ def realign_proj(cor_correction_only,
             # center_of_rotation_avg_second_part, _, offset_init_second_part = rot_center_avg(aligned_proj[zero_deg_idx_array[1]:, start_slice:end_slice], 
             #                                                                                 theta_idx_pairs_second_part, 
             #                                                                                 theta_array_second_part)
-            shifts_init_first_part, _, _ = phase_xcorr_manual(aligned_proj[0], np.fliplr(aligned_proj[zero_deg_idx_array[0]]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([-180, 0]))
-            shifts_init_second_part, _, _ = phase_xcorr_manual(aligned_proj[zero_deg_idx_array[1]], np.fliplr(aligned_proj[-1]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([0, 180]))
+            shifts_init_first_part, _, _ = phase_xcorr_manual(aligned_proj[0, start_slice:end_slice], np.fliplr(aligned_proj[zero_deg_idx_array[0], start_slice:end_slice]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([-180, 0]))
+            shifts_init_second_part, _, _ = phase_xcorr_manual(aligned_proj[zero_deg_idx_array[1], start_slice:end_slice], np.fliplr(aligned_proj[-1, start_slice:end_slice]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([0, 180]))
             
             center_geom = aligned_proj.shape[2]//2
 
