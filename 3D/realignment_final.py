a7933b17693da0048d87d70f92bed6574d8255dd
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-04-06 20:33:07 -0500

    Update realignment_final.py

--- 3D/realignment_final.py
+++ 3D/realignment_final.py
@@ -734,6 +734,7 @@ def realign_proj(cor_correction_only,
             shifts_init_first_part, phase_xcorr_first_part, _ = phase_xcorr_manual(aligned_proj[0, start_slice:end_slice], np.fliplr(aligned_proj[zero_deg_idx_array[0], start_slice:end_slice]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([-180, 0]))
             shifts_init_second_part, phase_xcorr_second_part, _ = phase_xcorr_manual(aligned_proj[zero_deg_idx_array[1], start_slice:end_slice], np.fliplr(aligned_proj[-1, start_slice:end_slice]), sigma = sigma, alpha = alpha, pixel_rad = 0, theta = np.array([0, 180]))
             
+            vert_shift_second_part  = shifts_init_second_part[0]
             center_geom = aligned_proj.shape[2]//2
 
             offset_init_first_part = shifts_init_first_part[1]/2
@@ -761,7 +762,7 @@ def realign_proj(cor_correction_only,
                     ddx = 0
                     net_x_shifts_pcc[0, :zero_deg_idx_array[1], start_slice:end_slice] -= offset_init_first_part
                     net_x_shifts_pcc[0, zero_deg_idx_array[1]:, start_slice:end_slice] -= (offset_init_second_part + ddx)
-                
+                    net_y_shifts_pcc[0, zero_deg_idx_array[1]:, start_slice:end_slice] -= vert_shift_second_part
                 else:
                     net_x_shifts_pcc[0, :zero_deg_idx_array[1]] -= offset_init_first_part
                     net_x_shifts_pcc[0, zero_deg_idx_array[1]:] -= offset_init_second_part
