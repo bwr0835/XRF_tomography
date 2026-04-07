647072267f47e90d3953778c16a901d684142917
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-04-06 20:14:59 -0500

    Update realignment_final.py

--- 3D/realignment_final.py
+++ 3D/realignment_final.py
@@ -757,7 +757,7 @@ def realign_proj(cor_correction_only,
                 print(f'Applying initial COR correction to pre-flipped, pre-remounted sample angles: {ppu.round_correct(-offset_init_first_part, ndec = 13)}')
 
                 if net_x_shifts_pcc.ndim == 3:
-                    ddx = -2
+                    ddx = 4
                     net_x_shifts_pcc[0, :zero_deg_idx_array[1], start_slice:end_slice] -= offset_init_first_part
                     net_x_shifts_pcc[0, zero_deg_idx_array[1]:, start_slice:end_slice] -= (offset_init_second_part + ddx)
                 
