36059b53b76154bae8670cbf63c9c813ea70bdb5
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-04-24 15:45:11 -0500

    BR

--- 3D/realignment_final_ultimate.py
+++ 3D/realignment_final_ultimate.py
@@ -499,7 +499,7 @@ def correct_center_of_rotation(proj_img_array,
             if sample_flipped_remounted_correction_type == 'differential':
                 print(f'Correcting sample remounting offset in shifted projection images: {ppu.round_correct(dy, ndec = 13)} pixels in y and {ppu.round_correct(dx/2, ndec = 13)} pixels in x for each half dataset...')
 
-                net_x_shift_array[:zero_deg_idx_array[1]] -= dx/2
+                net_x_shift_array[:zero_deg_idx_array[1]] += dx/2
                 net_x_shift_array[zero_deg_idx_array[1]:] += dx/2
             
             elif sample_flipped_remounted_correction_type == 'absolute':
