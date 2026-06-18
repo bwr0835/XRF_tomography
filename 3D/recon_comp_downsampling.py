79f54b78927c12534fc744425b209876f08cfb27
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-06-18 17:21:17 -0500

    Update recon_comp_downsampling.py

--- 3D/recon_comp_downsampling.py
+++ 3D/recon_comp_downsampling.py
@@ -678,8 +678,8 @@ for idx, downsample_factor_1 in enumerate(downsample_factors_1):
 
         sys.exit()
 
-    # middle_slice_recons[idx] = recon[idx, middle_slice]/downsample_factors_1[idx]
-    middle_slice_recons[idx] = recon[idx, middle_slice]
+    middle_slice_recons[idx] = recon[idx, middle_slice]/downsample_factors_1[idx]
+    # middle_slice_recons[idx] = recon[idx, middle_slice]
 
     if save_recon:
         print('Saving reconstruction and downsampled scan data to HDF5 file for middle slice...')
