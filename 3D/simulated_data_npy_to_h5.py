44482c1dcc2c0a8f3dca410a18fb6fe74cd876fc
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-07-16 11:09:13 -0500

    Update simulated_data_npy_to_h5.py

--- 3D/simulated_data_npy_to_h5.py
+++ 3D/simulated_data_npy_to_h5.py
@@ -20,11 +20,11 @@ dir_path = '/home/bwr0835'
 output_path_xrt = os.path.join(dir_path, 'simulated_proj_data_xrt_64_64_64.h5')
 
 # proj_data_xrf = np.zeros((4, 200, 64, 64))
-proj_data_xrt = np.zeros((1, 200, 64, 64))
+proj_data_xrt = np.zeros((1, 400, 64, 64))
 
 proj_data_xrt[0] = np.load(os.path.join(dir_path, 'simulated_proj_data_xrt_64_64_64.npy')).reshape(200, 64, 64)
 
-theta = np.linspace(0, 360, 200)
+theta = np.linspace(0, 360, 400)
 
 # elements_xrf = ['Ca', 'Ca_L', 'Sc', 'Sc_L']
 elements_xrt = ['xrt_sig']
@@ -43,10 +43,13 @@ with h5py.File(output_path_xrt, 'w') as f:
 
 fig1, axs1 = plt.subplots()
 
-axs1.imshow(proj_data_xrt[0, 0], cmap = 'jet', aspect = 'auto', extent = [0, 63, -177.8, 178.2])
+axs1.imshow(proj_data_xrt[0, 0], cmap = 'jet', aspect = 'auto', extent = [-0.5, 63.5, -180.45, 179.55])
+axs1.tick_params(axis = 'both', which = 'major', labelsize = 14)
 axs1.set_title(r'XRT')
 axs1.set_ylabel(r'$\theta$ (degrees)')
 axs1.set_xlabel(r'Scan position index')
+
+plt.show()
 
 # for theta_idx in range(200):
 #     file_path = f'{dir_path}/simulated_proj_data_xrf_no_probe_att_no_selfab_64_64_64_{theta_idx}.npy'
