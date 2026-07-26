f6e37e7d2a5f66512249d3b71a23b88de3d1319e
bwr0835 <benjaminroter2026@u.northwestern.edu>
2026-07-26 14:35:39 -0500

    Update xrf_xrt_preprocess_file_util.py

--- 3D/xrf_xrt_preprocess_file_util.py
+++ 3D/xrf_xrt_preprocess_file_util.py
@@ -2315,9 +2315,11 @@ def run_mpv(filename):
         print('Running MPV...')
         
         os.system('pkill -f mpv 2>/dev/null')
-        subprocess.Popen(['mpv', '--fs', '--keep-open=yes', filename])
+        subprocess.run(['mpv', '--fs', '--keep-open=yes', filename], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
     else:
         print('MPV is not supported on this platform.')
+
+        return
 
     return
